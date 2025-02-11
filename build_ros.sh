8e7df11d04b8ee1a70b474234427426d9656f7c9
Paul Taylor <123190211+uni-paul-taylor2@users.noreply.github.com>
2025-02-11 06:18:57 -0400

    Update build_ros.sh

--- build_ros.sh
+++ build_ros.sh
@@ -3,5 +3,5 @@ echo "Building ROS nodes"
 cd Examples/ROS/ORB_SLAM3
 mkdir build
 cd build
-cmake .. -DROS_BUILD_TYPE=Release -GNinja
+cmake .. -DROS_BUILD_TYPE=Release -GNinja -DCMAKE_INSTALL_PREFIX=/usr/local
 ninja -j $(nproc)
