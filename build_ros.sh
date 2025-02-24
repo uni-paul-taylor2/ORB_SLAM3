462d0921dc02c89a3bd55e3ef3b6b27ed688284b
Paul Taylor <123190211+uni-paul-taylor2@users.noreply.github.com>
2025-02-23 20:11:43 -0400

    ros build returns from `ninja` to `make`

--- build_ros.sh
+++ build_ros.sh
@@ -3,6 +3,6 @@ echo "Building ROS nodes"
 cd Examples/ROS/ORB_SLAM3
 mkdir build
 cd build
-cmake .. -DROS_BUILD_TYPE=Release -GNinja -DCMAKE_INSTALL_PREFIX=/usr/local
-ninja -j $(nproc)
+cmake .. -DROS_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/local
+make -j $(nproc)
 cd ..
